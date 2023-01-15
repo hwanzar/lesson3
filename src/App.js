ed6fe857b58e5fbf9552cfb19af4babf96cd08c3
gia <giadh1103@gmail.com>
2023-01-15 13:01:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+    {
+        id: 0,
+        name: "gia",
+        age: 20,
+    },
+    {
+        id: 1,
+        name: "abc",
+        age: 20,
+    },
+    {
+        id: 2,
+        name: "xyz",
+        age: 20,
+    },
+];
+
+function NameCard(props) {
+    return (
+        <div className="card">
+            <div className="name">Name: {props.name}</div>
+            <div className="age">Age: {props.age}</div>
+        </div>
+    );
 }
 
+const NameCard2 = (props) => {
+    return (
+        <div className="card">
+            <div className="name">Name: {props.name}</div>
+            <div className="age">Age: {props.age}</div>
+        </div>
+    );
+};
+// function NameCard(props) {
+//     return (
+
+//     );
+// }
+function App() {
+    return (
+        <div className="wrapper">
+            <div>{data.map((item) => NameCard(item))}</div>
+            <div>{data.map((item2) => NameCard2(item2))}</div>
+        </div>
+    );
+}
+// ReactDOM.render(
+//     <div>
+//         <NameCard name="zarr" age="20" />
+//         <NameCard name="Bob" age="20" />
+//         <NameCard name="Cris" age="20" />
+//     </div>,
+//     document.getElementById("root") //WHAT IS THIS
+// );
 export default App;
